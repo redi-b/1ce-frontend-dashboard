abec3b62d85a9370d4a4e61fca2851b43dc2b432
rediet <rb.rediet@gmail.com>
2025-07-10 12:01:34 +0300

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+
+	import { ModeWatcher } from 'mode-watcher';
+
+	import '@fontsource-variable/inter';
+	import '@fontsource-variable/manrope';
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher modeStorageKey="1ce-theme" themeStorageKey="1ce-color-scheme" />
+
+{@render children()}
